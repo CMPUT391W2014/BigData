8263668bc0a31f780e1e57753395d8ef1acaa54b
Ben Dubois <tbendubois@gmail.com>
2014-03-12 12:41:36 -0600

    Added C Code which will generate Java Files

---
+++ genJavaCode.c
@@ -0,0 +1,71 @@
+#include <err.h>
+#include <errno.h>
+#include <limits.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+/* Ben Dubois
+ * a program to generate java code for cassandra SSTable creation machine
+ * I'm coding it to do an output for the following type:
+ * 
+ * First:
+ * with key_validation_class=LexicalUUIDType
+    and comparator=AsciiType
+    and column_metadata=[
+        { column_name: 'firstname', validation_class: AsciiType }
+        { column_name: 'lastname', validation_class: AsciiType }
+        { column_name: 'password', validation_class: AsciiType }
+        { column_name: 'age', validation_class: LongType, index_type: KEYS }
+        { column_name: 'email', validation_class: AsciiType }];
+ * 
+ * Then:
+ * 
+ * for (...each csv entry...)
+ * ByteBuffer uuid = ByteBuffer.wrap(decompose(entry.key));
+ * usersWriter.newRow(uuid);
+ * usersWriter.addColumn(bytes("firstname"), bytes(entry.firstname), timestamp); 
+ * -------
+ * 
+ * As per the page at: 
+ * http://www.datastax.com/dev/blog/bulk-loading
+*/
+static void validate() {
+	char *input, *saveptr1;
+	char *varName, *varType, *line, *ptr;
+	FILE *sql;
+	FILE *outCode;
+	outCode = fopen("outputCode.txt", "w");
+	static const char filename[] = "bigdata_setup1.sql";
+	varName = (char*)malloc(sizeof(char)*256);
+	varType = (char*)malloc(sizeof(char)*256);
+	input = (char*)malloc(sizeof(char)*256);
+	fprintf(outCode, "with key_validation_class=LexicalUUIDType\nand comparator=AsciiType\nand column_metadata=[\n");
+	sql = fopen ( filename, "r" );
+	while(fgets(input, 256, sql) != 0) {
+		varName = strtok_r(input, " ", &saveptr1);
+		//Grabs first string
+		if (varName == NULL) {
+			fprintf(stderr, "no variable name read\n");
+		}
+		//Grabs second string
+		varType = strtok_r(NULL, " ", &saveptr1);
+		if (varType == NULL) {
+			fprintf(stderr, "no variable type read\n");
+		}
+		// This line gets changed to format the code output
+		//***
+		fprintf(outCode, "\t{ column_name: '%s', validation_class: %s }", varName, varType);
+		//***
+	}
+	free(varName);
+	free(varType);
+}
+
+int main() {
+	validate();
+	return 1;
+}
